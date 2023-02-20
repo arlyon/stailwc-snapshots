613f23c3144ff10e028151241ce8ed6cf480b486
Alexander Lyon <arlyon@me.com>
2023-02-20 16:41:05 +0000

    update snapshots for grid-template-*

--- output/gridTemplateRows.js
+++ output/gridTemplateRows.js
@@ -1,9 +1,4 @@
-
-
-import tw, { theme } from './macro'
-
 // https://tailwindcss.com/docs/grid-template-rows
-theme`gridTemplateRows`
 
 tw`grid-rows-1`
 tw`grid-rows-2`
@@ -19,37 +14,28 @@ tw`grid-rows-[200px minmax(900px, 1fr) 100px]`
 
 // https://tailwindcss.com/docs/grid-template-rows
 ({
-  1: 'repeat(1, minmax(0, 1fr))',
-  2: 'repeat(2, minmax(0, 1fr))',
-  3: 'repeat(3, minmax(0, 1fr))',
-  4: 'repeat(4, minmax(0, 1fr))',
-  5: 'repeat(5, minmax(0, 1fr))',
-  6: 'repeat(6, minmax(0, 1fr))',
-  none: 'none',
+  gridTemplateRows: "repeat(1, minmax(0, 1fr))",
 });
 ({
-  gridTemplateRows: 'repeat(1, minmax(0, 1fr))',
+  gridTemplateRows: "repeat(2, minmax(0, 1fr))",
 });
 ({
-  gridTemplateRows: 'repeat(2, minmax(0, 1fr))',
+  gridTemplateRows: "repeat(3, minmax(0, 1fr))",
 });
 ({
-  gridTemplateRows: 'repeat(3, minmax(0, 1fr))',
+  gridTemplateRows: "repeat(4, minmax(0, 1fr))",
 });
 ({
-  gridTemplateRows: 'repeat(4, minmax(0, 1fr))',
+  gridTemplateRows: "repeat(5, minmax(0, 1fr))",
 });
 ({
-  gridTemplateRows: 'repeat(5, minmax(0, 1fr))',
+  gridTemplateRows: "repeat(6, minmax(0, 1fr))",
 });
 ({
-  gridTemplateRows: 'repeat(6, minmax(0, 1fr))',
+  gridTemplateRows: "none",
 });
 ({
-  gridTemplateRows: 'none',
-});
-({
-  gridTemplateRows: '200px minmax(900px, 1fr) 100px',
+  gridTemplateRows: "200px minmax(900px, 1fr) 100px",
 })
 
 
