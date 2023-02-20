1c44e8f529284e3e019968279013979a9a38446f
Alexander Lyon <arlyon@me.com>
2023-02-20 22:49:46 +0000

    update snapshot for columns

--- output/columns.js
+++ output/columns.js
@@ -1,9 +1,4 @@
-
-
-import tw, { theme } from './macro'
-
 // https://tailwindcss.com/docs/columns
-theme`columns`
 
 tw`columns-1`
 tw`columns-2`
@@ -38,113 +33,85 @@ tw`columns-[10rem]`
 
 // https://tailwindcss.com/docs/columns
 ({
-  1: '1',
-  2: '2',
-  3: '3',
-  4: '4',
-  5: '5',
-  6: '6',
-  7: '7',
-  8: '8',
-  9: '9',
-  10: '10',
-  11: '11',
-  12: '12',
-  auto: 'auto',
-  '3xs': '16rem',
-  '2xs': '18rem',
-  xs: '20rem',
-  sm: '24rem',
-  md: '28rem',
-  lg: '32rem',
-  xl: '36rem',
-  '2xl': '42rem',
-  '3xl': '48rem',
-  '4xl': '56rem',
-  '5xl': '64rem',
-  '6xl': '72rem',
-  '7xl': '80rem',
+  columns: "1",
 });
 ({
-  columns: '1',
+  columns: "2",
 });
 ({
-  columns: '2',
+  columns: "3",
 });
 ({
-  columns: '3',
+  columns: "4",
 });
 ({
-  columns: '4',
+  columns: "5",
 });
 ({
-  columns: '5',
+  columns: "6",
 });
 ({
-  columns: '6',
+  columns: "7",
 });
 ({
-  columns: '7',
+  columns: "8",
 });
 ({
-  columns: '8',
+  columns: "9",
 });
 ({
-  columns: '9',
+  columns: "10",
 });
 ({
-  columns: '10',
+  columns: "11",
 });
 ({
-  columns: '11',
+  columns: "12",
 });
 ({
-  columns: '12',
+  columns: "auto",
 });
 ({
-  columns: 'auto',
+  columns: "16rem",
 });
 ({
-  columns: '16rem',
+  columns: "18rem",
 });
 ({
-  columns: '18rem',
+  columns: "20rem",
 });
 ({
-  columns: '20rem',
+  columns: "24rem",
 });
 ({
-  columns: '24rem',
+  columns: "28rem",
 });
 ({
-  columns: '28rem',
+  columns: "32rem",
 });
 ({
-  columns: '32rem',
+  columns: "36rem",
 });
 ({
-  columns: '36rem',
+  columns: "42rem",
 });
 ({
-  columns: '42rem',
+  columns: "48rem",
 });
 ({
-  columns: '48rem',
+  columns: "56rem",
 });
 ({
-  columns: '56rem',
+  columns: "64rem",
 });
 ({
-  columns: '64rem',
+  columns: "72rem",
 });
 ({
-  columns: '72rem',
+  columns: "80rem",
 });
 ({
-  columns: '80rem',
-});
-({
-  columns: '10rem',
+  columns: "10rem",
 })
 
 
